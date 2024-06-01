56d4ea1b7ab152de5e7812413ac7a26881718c2d
syarifanurfadilah <105841107821@student.unismuh.ac.id>
2024-06-01 21:53:33 +0800

    Tugas 3 lab

--- App.js
+++ App.js
@@ -1,83 +1,170 @@
-// import { StyleSheet, Text, View } from 'react-native'
-// import React from 'react'
+// // import { StyleSheet, Text, View } from 'react-native'
+// // import React from 'react'
+
+const { View } = require("react-native")
+
+// // const App = () => {
+// //   return (
+// //    <View style ={{
+// //     flex:1,
+// //     }}>
+// //       <View style ={{flex: 1, flexDirection: "row"}}>
+// //         <View style={{flex:1, backgroundColor:'yellow'}}></View>
+// //         <View style={{flex:1, backgroundColor:'green'}}></View>
+
+
+// //       </View> 
+// //       <View style={{flex:1, backgroundColor:'red'}}></View>
+// //       <View style={{flex:1, backgroundColor:'blue'}}></View>
+      
+        
+        
+        
+
+// //    </View>
+// //   )
+// // }
+// // export default App
+
+// import { StyleSheet,Text, View } from "react-native";
+// import React from 'react';
 
 // const App = () => {
 //   return (
-//    <View style ={{
-//     flex:1,
-//     }}>
-//       <View style ={{flex: 1, flexDirection: "row"}}>
-//         <View style={{flex:1, backgroundColor:'yellow'}}></View>
-//         <View style={{flex:1, backgroundColor:'green'}}></View>
-
-
-//       </View> 
-//       <View style={{flex:1, backgroundColor:'red'}}></View>
-//       <View style={{flex:1, backgroundColor:'blue'}}></View>
+//     <View style={{flex : 1}}>
+//      <View style={{
       
-        
-        
-        
+//       flex: 1, 
+//       alignItems: 'flex-end', //BARIS
+//       justifyContent: 'center', //KOLOM
+//       flexDirection: 'row',
+//       marginBottom: 100,
+//       justifyContent: "space-evenly"
 
-//    </View>
-//   )
+      
+
+//     }}>
+//       <View style ={{
+//         backgroundColor: 'red', width: 180, height: 80,
+//         justifyContent: 'center',
+//         borderRadius: 10,
+         
+      
+//       }}>
+        
+//         <Text style= {{
+//           fontSize: 30, color: 'white',
+//           textAlign: 'center',
+//           fontWeight: 'bold',
+//         }}>
+
+//           login
+//            </Text>
+//       </View>
+
+//       <View style ={{
+//         backgroundColor: 'blue', width: 180, height: 80,
+//         justifyContent: 'center',
+//         borderRadius: 10,
+        
+//       }}>
+        
+//         <Text style= {{
+//           fontSize: 30, color: 'white',
+//           textAlign: 'center',
+//           fontWeight: 'bold',
+//         }}>
+
+//           Register
+//            </Text>
+//       </View>
+//      </View>
+//      </View>
+//   );
 // }
 // export default App
 
-import { StyleSheet,Text, View } from "react-native";
-import React from 'react';
+import React from "react";
+import{StyleSheet,Text,Image} from 'react-native';
 
-const App = () => {
-  return (
-    <View style={{flex : 1}}>
-     <View style={{
-      
-      flex: 1, 
-      alignItems: 'flex-end', //BARIS
-      justifyContent: 'center', //KOLOM
-      flexDirection: 'row',
-      marginBottom: 100,
-      justifyContent: "space-evenly"
 
-      
+const App = () =>{
+  return(
+  <View style={{flex:1}}>
+    <View style= {{
+      flex : 1,
+      alignItems : 'center',
+      justifyContent: 'center',
 
     }}>
-      <View style ={{
-        backgroundColor: 'red', width: 180, height: 80,
-        justifyContent: 'center',
-        borderRadius: 10,
-         
-      
-      }}>
-        
-        <Text style= {{
-          fontSize: 30, color: 'white',
-          textAlign: 'center',
-          fontWeight: 'bold',
-        }}>
+      <Text style={{
+        fontSize : 50,
+        fontWeight :'bold'
 
-          login
-           </Text>
+      }}>Mulai</Text>
       </View>
 
-      <View style ={{
-        backgroundColor: 'blue', width: 180, height: 80,
+      <View style={{
+        flex :1,
+        alignItems: 'center',
         justifyContent: 'center',
-        borderRadius: 10,
         
       }}>
-        
-        <Text style= {{
-          fontSize: 30, color: 'white',
-          textAlign: 'center',
-          fontWeight: 'bold',
-        }}>
+        <Image
+         source={require('./shopee-logo.png')}
+         style= {StyleS.Image}
+         /> 
+       </View>
 
-          Register
-           </Text>
+       <View style={{
+        flex: 1,
+        alignItems:'center',
+        justifyContent:'center',
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+     }}>
+
+        <View style={{
+          backgroundColor:'red',
+          width: 115,
+          height: 75,
+          borderRadius: 25,
+          justifyContent:'center',
+
+        }}>
+          <Text style={{
+            textAlign:'center',
+            fontSize:25,
+            color:'white',
+            fontWeight:'bold',
+          }}>Masuk </Text>
+          </View>
+
+          <View style={{
+            backgroundColor:'black',
+            width:115,
+            height:75,
+            borderRadius:25,
+            justifyContent:'center',
+         }}>
+            <Text style={{
+              textAlign:'center',
+              fontSize: 25,
+              color:'white',
+              fontWeight:'bold',
+            }}>Daftar
+            </Text>
+          </View>
       </View>
-     </View>
-     </View>
+    </View>
   );
 }
-export default App
+
+const StyleS = StyleSheet.create({
+  Image: {
+    width:300,
+    height:300,
+    resizeMode:'contain',
+  },
+});
+export default App;
